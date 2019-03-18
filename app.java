af087c4a3d578f0e3232aa22cd2032a1ba6e1589
sudheer <sudheer.ganatech308@gmail.com>
2019-03-18 09:40:29 +0530

    1

---
+++ app.java
@@ -0,0 +1,20 @@
+public class EqualMatrix    
+{    
+    public static void main(String[] args) {    
+        int row1, col1, row2, col2;    
+        boolean flag = true;    
+            
+        //Initialize matrix a    
+        int a[][] = {       
+                        {1, 2, 3},    
+                        {8, 4, 6},    
+                        {4, 5, 7}    
+     }    
+                
+            if(flag)    
+                System.out.println("Matrices are equal");    
+            else    
+                System.out.println("Matrices are not equal");    
+        }    
+    }    
+}    
